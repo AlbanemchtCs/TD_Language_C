64b75d09fc37623734facde6125c634fc4a68232
Albane Michot <albane.michot@student-cs.fr>
2022-10-30 18:22:53 +0100

    Part 3: Concrete class

--- tp/clp_tp2/TestNumber.cpp
+++ tp/clp_tp2/TestNumber.cpp
@@ -39,6 +39,64 @@ TEST( TestNumber, TestNumberBig )
     EXPECT_EQ( os.str(), "12345123451234512345" );
 }
 
+// Tests constructor by copy
+TEST( TestNumber, TestNumberConstructorCopy0 )
+{
+    Number n{ 0 };
+    Number n_const(n);
+    std::ostringstream os;
+    os << n_const;
+    EXPECT_EQ( os.str(), "0" );
+}
+
+TEST( TestNumber, TestNumberConstructorCopy23456789 )
+{
+    Number n{ 23456789 };
+    Number n_const(n);
+    std::ostringstream os;
+    os << n_const;
+    EXPECT_EQ(os.str(), "23456789");
+}
+
+TEST( TestNumber, TestNumberConstructorCopyBig )
+{
+    Number n{ 2345623456234 };
+    Number n_const(n);
+    std::ostringstream os;
+    os << n_const;
+    EXPECT_EQ( os.str(), "2345623456234" );
+}
+
+// Tests affectation by copy
+TEST( TestNumber, TestNumberAffectationCopy0 )
+{
+    Number n{ 0 };
+    Number n_affected(n);
+    std::ostringstream os;
+    os << n_affected;
+    EXPECT_EQ( os.str(), "0" );
+}
+
+TEST( TestNumber, TestNumberAffectationCopy23456789 )
+{
+    Number n{ 23456789 };
+    Number n_affected(n);
+    std::ostringstream os;
+    os << n_affected;
+    EXPECT_EQ(os.str(), "23456789");
+
+}
+
+TEST( TestNumber, TestNumberAffectationCopyBig )
+{
+    Number n{ 2345623456234 };
+    Number n_affected(n);
+    std::ostringstream os;
+    os << n_affected;
+    EXPECT_EQ( os.str(), "2345623456234" );
+}
+
+
 int main( int argc, char * argv[] )
 {
     ::testing::InitGoogleTest( &argc, argv );

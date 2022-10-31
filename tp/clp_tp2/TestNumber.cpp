aa050d05015260aad7b0307809c045622091befa
Albane Michot <albane.michot@student-cs.fr>
2022-10-31 12:41:54 +0100

    Part 4: calculations

--- tp/clp_tp2/TestNumber.cpp
+++ tp/clp_tp2/TestNumber.cpp
@@ -96,6 +96,72 @@ TEST( TestNumber, TestNumberAffectationCopyBig )
     EXPECT_EQ( os.str(), "2345623456234" );
 }
 
+// Tests add
+TEST( TestNumber, TestNumberAdd0 )
+{
+    Number n{0};
+    n.add(7654);
+    std::ostringstream os;
+    os << n;
+    EXPECT_EQ(os.str(), "7654");
+}
+
+TEST( TestNumber, TestNumberAdd7654 )
+{
+    Number n{ 7654 };
+    n.add(500);
+    std::ostringstream os;
+    os << n;
+    EXPECT_EQ(os.str(), "8154");
+}
+
+TEST( TestNumber, TestNumberAddBig76543210987 )
+{
+    Number n{ 76543210987 };
+    n.add(3);
+    std::ostringstream os;
+    os << n;
+    EXPECT_EQ(os.str(), "76543210990");
+}
+
+// Tests multiply
+TEST( TestNumber, TestNumberMultiply0 )
+{
+    Number n{0};
+    n.multiply(150);
+    std::ostringstream os;
+    os << n;
+    EXPECT_EQ(os.str(), "0");
+}
+
+TEST( TestNumber, TestNumberMultiply7654 )
+{
+    Number n{7654};
+    n.multiply(2);
+    std::ostringstream os;
+    os << n;
+    EXPECT_EQ(os.str(), "15308");
+
+}
+
+TEST( TestNumber, TestNumberMultiplyBig76543210987 )
+{
+    Number n{76543210987};
+    n.multiply(12);
+    std::ostringstream os;
+    os << n;
+    EXPECT_EQ(os.str(), "918518531844");
+}
+
+// Tests factorial
+TEST( TestNumber, TestFactorial123 )
+{
+    std::ostringstream os;
+    os << factorial( 123 );;
+    EXPECT_EQ( os.str(), "121463043670253296757662432418812958554542170884833823153289181618292"
+                         "358923621676688311569606126402021707358352212940477825910915704116514"
+                         "72186029519906261646730733907419814952960000000000000000000000000000" );
+}
 
 int main( int argc, char * argv[] )
 {
